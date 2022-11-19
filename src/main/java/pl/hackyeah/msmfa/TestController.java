fe7fba5d706d6f99b3ef642a4fe4bad889aaea32
Tomasz Kańka <tomasz.kanka@deviniti.com>
2022-11-19 12:04:35 +0100

    y

--- src/main/java/pl/hackyeah/msmfa/TestController.java
+++ src/main/java/pl/hackyeah/msmfa/TestController.java
@@ -1,16 +1,24 @@
 package pl.hackyeah.msmfa;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import pl.hackyeah.msmfa.dto.TestDTO;
+import pl.hackyeah.msmfa.entity.TestEntity;
+import pl.hackyeah.msmfa.repository.TestRepository;
 
 @RestController
 public class TestController {
 
+	@Autowired
+	private TestRepository repository;
+	
 	@GetMapping("/t")
-	public TestDTO test() {
-		TestDTO dto = new TestDTO(1l, this.toString());
-		return dto;
+	public TestEntity test() {
+
+		Optional<TestEntity> e = repository.findById(1l);
+		return e.get();
 	}
 }
