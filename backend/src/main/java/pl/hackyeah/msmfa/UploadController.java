8c8fb92dd550f9ebac64a0e9ac7c77ef252cc42d
Tomasz Kańka <tomasz.kanka@deviniti.com>
2022-11-19 20:20:52 +0100

    upload

--- backend/src/main/java/pl/hackyeah/msmfa/UploadController.java
+++ backend/src/main/java/pl/hackyeah/msmfa/UploadController.java
@@ -4,20 +4,21 @@ import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.util.Set;
 
-import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import com.google.rpc.context.AttributeContext.Response;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import lombok.extern.slf4j.Slf4j;
+import pl.hackyeah.msmfa.dto.TestDTO;
 import pl.hackyeah.msmfa.service.LogoService;
 
-@Controller
+@RestController
 @Slf4j
 public class UploadController {
 
@@ -26,18 +27,24 @@ public class UploadController {
 	private LogoService logoService;
 	
 	@PostMapping("/upload")
-	public Response handleFileUpload(@RequestParam("file") MultipartFile file,
+	public TestDTO handleFileUpload(@RequestParam("file") MultipartFile file,
 			RedirectAttributes redirectAttributes) throws IOException {
 
-		byte[] buf = new byte[1000000];
-		IOUtils.readFully(file.getInputStream(), buf );
-		Set<String> logos = logoService.detectLogo( new ByteArrayInputStream(buf) );
-		String text = logoService.ocr(new ByteArrayInputStream(buf));
+		Set<String> logos = logoService.detectLogo( new ByteArrayInputStream(file.getBytes()) );
+		String text = logoService.ocr(new ByteArrayInputStream(file.getBytes()));
 
 		
 		System.err.println("LOGOS: " + logos);
 		System.err.println("OCR: " + text); 
 		
-		return Response.newBuilder().setCode(200).build();
+		TestDTO dto = new TestDTO(1l, logos, text);
+		ObjectMapper mapper = new ObjectMapper();
+		String s = mapper.writeValueAsString(dto);
+		redirectAttributes.addAttribute("found", s);
+		
+		return dto;  
 	}
+	
+	
+	
 }
--- backend/src/main/java/pl/hackyeah/msmfa/dto/TestDTO.java
+++ backend/src/main/java/pl/hackyeah/msmfa/dto/TestDTO.java
@@ -1,7 +1,10 @@
 package pl.hackyeah.msmfa.dto;
 
+import java.util.Collection;
+
 public record TestDTO(
 		Long id,
+		Collection<String> logos,
 		String text) {
 	
 }
