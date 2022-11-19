d97ff1809a1e2cc309d92d7e9695593c66361378
Daniel <daniel.czajka@deviniti.com>
2022-11-19 19:47:24 +0100

    refactor

--- backend/src/main/java/pl/hackyeah/msmfa/socialPost/SocialPostController.java
+++ backend/src/main/java/pl/hackyeah/msmfa/socialPost/SocialPostController.java
@@ -1,4 +1,31 @@
 package pl.hackyeah.msmfa.socialPost;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/socialPosts")
 public class SocialPostController {
+
+    @Autowired
+    private final SocialPostService socialPostService;
+
+    public SocialPostController(SocialPostService socialPostService) {
+        this.socialPostService = socialPostService;
+    }
+
+    @GetMapping("/find/all")
+    public ResponseEntity<List<SocialPostEntity>> getAllSocialPosts() {
+        List<SocialPostEntity> socialPosts = socialPostService.findAllSocialPosts();
+        return new ResponseEntity<>(socialPosts, HttpStatus.OK);
+    }
+
+    @GetMapping("/find/{id}")
+    public SocialPostEntity getSocialPostById(@PathVariable("id") Long id) {
+        return socialPostService.findById(id);
+    }
 }
--- backend/src/main/java/pl/hackyeah/msmfa/socialPost/SocialPostService.java
+++ backend/src/main/java/pl/hackyeah/msmfa/socialPost/SocialPostService.java
@@ -1,4 +1,30 @@
 package pl.hackyeah.msmfa.socialPost;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class SocialPostService {
+
+    @Autowired
+    private final SocialPostRepository socialPostRepository;
+
+    public SocialPostService(SocialPostRepository socialPostRepository) {
+        this.socialPostRepository = socialPostRepository;
+    }
+
+    public List<SocialPostEntity> findAllSocialPosts() {
+        List<SocialPostEntity> socialPosts = socialPostRepository.findAll();
+        return socialPosts;
+    }
+
+    public SocialPostEntity findById(Long id) {
+        return socialPostRepository.findById(id).get();
+    }
+
+    public SocialPostEntity addSocialPost(SocialPostEntity socialPost) {
+        return socialPostRepository.save(socialPost);
+    }
 }
