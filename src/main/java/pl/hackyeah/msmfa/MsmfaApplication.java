55efaadcf5f7c9e52d0c1fbebdafc62ef74807ed
Tomasz Kańka <tomasz.kanka@deviniti.com>
2022-11-19 11:16:36 +0100

    Initial commit

---
+++ src/main/java/pl/hackyeah/msmfa/MsmfaApplication.java
@@ -0,0 +1,13 @@
+package pl.hackyeah.msmfa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsmfaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsmfaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/hackyeah/msmfa/MsmfaApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.hackyeah.msmfa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsmfaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
