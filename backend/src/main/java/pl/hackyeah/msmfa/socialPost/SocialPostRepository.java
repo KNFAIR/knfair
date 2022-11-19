54e24aba05fd6bcce32654e8ef43dc7582ce0fdf
Daniel <daniel.czajka@deviniti.com>
2022-11-19 21:18:59 +0100

    refactor

--- backend/src/main/java/pl/hackyeah/msmfa/socialPost/SocialPostRepository.java
+++ backend/src/main/java/pl/hackyeah/msmfa/socialPost/SocialPostRepository.java
@@ -2,5 +2,11 @@ package pl.hackyeah.msmfa.socialPost;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface SocialPostRepository extends JpaRepository<SocialPostEntity, Long> {
+
+    List<SocialPostEntity> findByManualVerificationIsNullAndAutoVerificationIsTrue();
+
+    List<SocialPostEntity> findByManualVerificationIsNullAndAutoVerificationIsFalse();
 }
