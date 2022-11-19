da3a39eebdcae016a3baba6fd7e0235b217b78a6
Daniel <daniel.czajka@deviniti.com>
2022-11-19 15:22:28 +0100

    working on financial entity

--- backend/src/main/java/pl/hackyeah/msmfa/financialEntity/FinancialEntityController.java
+++ backend/src/main/java/pl/hackyeah/msmfa/financialEntity/FinancialEntityController.java
@@ -1,9 +1,30 @@
 package pl.hackyeah.msmfa.financialEntity;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/financialEntities")
 public class FinancialEntityController {
+
+    @Autowired
+    private final FinancialEntityService financialEntityService;
+
+    public FinancialEntityController(FinancialEntityService financialEntityService) {
+        this.financialEntityService = financialEntityService;
+    }
+
+
+    @GetMapping("/find/all")
+    public ResponseEntity<List<FinancialEntity>> getAllFinancialEntities() {
+        List<FinancialEntity> financialEntities = financialEntityService.findAllFinancialEntities();
+
+        return new ResponseEntity<>(financialEntities, HttpStatus.OK);
+    }
 }
